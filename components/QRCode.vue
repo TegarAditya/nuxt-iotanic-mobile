ab2c873edccdf74db7394c38d9a9ffd8ba3c18ef
Tegar Aditya <tegaraditya53@gmail.com>
2024-09-02 19:33:14 +0700

    feat: QRCode components

---
+++ components/QRCode.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="rounded-2xl overflow-hidden">
+    <VueQrcode :value="$props.value" />
+  </div>
+</template>
+
+<script lang="ts" setup>
+import VueQrcode from "@chenfengyuan/vue-qrcode"
+
+defineProps({
+  value: {
+    type: String,
+    required: true,
+  },
+})
+</script>
+
+<style></style>
