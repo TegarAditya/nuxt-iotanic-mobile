7f01a35e8d5c3f7bfe7e6c93cc676c2407192c0c
Tegar Aditya <tegaraditya53@gmail.com>
2024-09-02 19:30:33 +0700

    refractor: separate GradientComponent

---
+++ components/CircleGradientAccent.vue
@@ -0,0 +1,11 @@
+<template>
+  <div
+    class="gradient-circle absolute -left-56 top-5 aspect-square w-96 opacity-65"
+  />
+  <div
+    class="gradient-circle absolute -right-56 top-52 aspect-square w-96 opacity-65"
+  />
+  <div
+    class="gradient-circle absolute -bottom-52 -left-56 aspect-square w-96 opacity-65"
+  />
+</template>
